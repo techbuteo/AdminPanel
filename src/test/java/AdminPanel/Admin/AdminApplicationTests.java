8e8bece15f16783f41cee93a5d4bf496024c777f
Buteo <islomovabdurasul@gmail.com>
2021-02-06 22:35:49 +0500

    Spring bootda yangi proekt yaratildi

---
+++ src/test/java/AdminPanel/Admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package AdminPanel.Admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
